7dd3692d3327840df23667f259b3e2b47fae6ccc
shristi555 <shreesti500@gmail.com>
2025-11-19 14:50:15 +0545

    Added complaints page

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
 			location: "Near City Hall, 123 Main St",
 			timeAgo: "2 days ago",
 			likes: 15,
-			image: "https://lh3.googleusercontent.com/aida-public/AB6AXuCGDRcQfE90rTrAjxQZai_cdhDmAyIDfGlrhtY-VQIWDEJKmyU-ICrHeqAD7oSuptSUYVN88W8oV-n_wl-OuZFQdpHiLTHhqA9P0bCZ0mEbHtL1Hmzcx1F4raCtTkm4-4RDw_Zs_99Tc7g3130UJL6oilDUxJrBHtGRdQFMtgpGTo6qyybr8wr5EdJ7bNL_Qcktzhrw9QiH5AK4LZCnh7IR0zReZBl9sHaJ1slyHsHGNVQW0EtvbOWfOip42TCKrKdXP-Zm3YipekS",
+			image: "https://lh3.googleusercontent.com/aida-public/AB6AXuCGDRcQfE90rTrAjxQZai_cdhDmAyIDfGlrhtY-VQIWDEJKmyU-ICrHeqAD7oSuptSUYVN88W8oV-n_wl-OuZFQdpHiLTHhqA9P0bCZ0mEbHtL1Hmzcx1F4raCtTkm4-4RDw_Zs_99Tc7g3130UJL6oilDUxJrBHtGRdQFMtgpGTo6qyYybr8wr5EdJ7bNL_Qcktzhrw9QiH5AK4LZCnh7IR0zReZBl9sHaJ1slyHsHGNVQW0EtvbOWfOip42TCKrKdXP-Zm3YipekS",
 			imageAlt:
 				"A large, deep pothole on an asphalt street next to a curb.",
 		},
@@ -64,9 +64,21 @@ export default function Home() {
 			location: "General Hospital, 789 Health St",
 			timeAgo: "3 days ago",
 			likes: 30,
-			image: "https://lh3.googleusercontent.com/aida-public/AB6AXuCGDRcQfE90rTrAjxQZai_cdhDmAyIDfGlrhtY-VQIWDEJKmyU-ICrHeqAD7oSuptSUYVN88W8oV-n_wl-OuZFQdpHiLTHhqA9P0bCZ0mEbHtL1Hmzcx1F4raCtTkm4-4RDw_Zs_99Tc7g3130UJL6oilDUxJrBHtGRdQFMtgpGTo6qyYybr8wr5EdJ7bNL_Qcktzhrw9QiH5AK4LZCnh7IR0zReZBl9sHaJ1slyHsHGNVQW0EtvbOWfOip42TCKrKdXP-Zm3YipekS",
+			image: "",
 			imageAlt: "MRI scan showing areas of brain damage.",
 		},
+		{
+			id: 5,
+			category: "Water Supply",
+			categoryColor: "teal",
+			title: "Contaminated Drinking Water",
+			status: "Reported",
+			statusColor: "orange",
+			location: "Maplewood Residential Area",
+			timeAgo: "4 days ago",
+			likes: 12,
+			image: "https://static.vecteezy.com/system/resources/thumbnails/002/187/276/small/mineral-drinking-water-on-wood-table-and-water-in-glass-photo.jpg"
+		}
 	];
 
 	return (
